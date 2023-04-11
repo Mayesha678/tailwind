7a70e2533042fcafee689ad56f2c69691b7b6723
mayesha35 <119776219+mayesha35@users.noreply.github.com>
2023-04-12 01:07:13 +0600

    ..

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     './src/**/*.html',
+//     './**/*.html',
+//     'index.html',
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+// module.exports = {
+//   purge: [],
+//   darkMode: false, // or 'media' or 'class'
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
